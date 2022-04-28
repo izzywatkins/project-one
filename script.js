6d4a921339e32e0f4b1745c2d331f137178c4f25
izzywatkins <izabella6422@gmail.com>
2022-04-28 11:22:25 -0700

    worked on autoscroll

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
-function animate() {
-    $container = $('#text-effect');
-    const text = $container.text();
-    const $elements = text.split('').map(s => $(`<span>${s}</span>`));
+//document.getElementById('one').scrollTo({ behavior: 'smooth', top: 0 })
+//$('#go').click(function () {
+  setTimeout(function () {
+    element = document.getElementById('two')
+    element.scrollIntoView()
+  }, 2000)
   
-    $container.html($elements);
-    $container.show();
   
-    $elements.forEach(function ($el, i) {
-      $el.
-      css({ top: -60, opacity: 0 }).
-      delay(40 * i).
-      animate({ top: 0, opacity: 1 }, 500, 'easeOutExpo');
-    });
-  }
-  $(function () {
-    animate();
-  });
+  setTimeout(function () {
+    element = document.getElementById('three')
+    element.scrollIntoView()
+  }, 4000)
+  
+  setTimeout(function () {
+    element = document.getElementById('four')
+    element.scrollIntoView()
+  }, 6000)
+  //})
+  //  var container = $('section')
+  
+  //  // Scrolls to paragraph 1
+  //  function scrollSection2() {
+  //    var scrollTo = $('#two')
+  
+  //    // Calculating new position
+  //    // of scrollbar
+  //    var position = scrollTo.offset().top - container.offset().top + container.scrollTop()
+  
+  //    // Animating scrolling effect
+  //    container.animate({
+  //      scrollTop: position
+  //    })
+  //  }
+  
+  //  // Scrolls to paragraph 2
+  //  function scrollSection3() {
+  //    var scrollTo = $('#three')
+  
+  //    // Calculating new position
+  //    // of scrollbar
+  //    var position = scrollTo.offset().top - container.offset().top + container.scrollTop()
+  
+  //    // Animating scrolling effect
+  //    container.animate({
+  //      scrollTop: position
+  //    })
+  //  }
