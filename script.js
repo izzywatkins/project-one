8ad427b424e2024cbaa726c8291a38501afeb951
izzywatkins <izabella6422@gmail.com>
2022-05-26 10:08:19 -0700

    animations!

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 //document.getElementById('one').scrollTo({ behavior: 'smooth', top: 0 })
 //$('#go').click(function () {
 
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
 $('#start-btn').click (function () {
   setTimeout(function () {
     element = document.getElementById('two')
